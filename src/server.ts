84e08b40de43e27888656b217a02d74c9d2b7381
Aditya <randomhimalayanguy@gmail.com>
2025-11-21 02:31:10 +0530

    Testing

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import app from './app';
 import { connectDatabase } from './config/database';
 import { config } from './config/environment';
 
+console.log('MongoDB URI:', config.mongoDBURI);
+
 connectDatabase().then(() => {
   app.listen(config.port, () => {
-    console.log(`Server started on: http://localhost:${config.port}`);
+    console.log(`Server started`);
   });
 });
