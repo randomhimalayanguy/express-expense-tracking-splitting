18596a8ae64239dc3a786ee63ac742bee5936e19
Aditya <randomhimalayanguy@gmail.com>
2025-11-21 01:33:03 +0530

    Important changes

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-    port : process.env.PORT || 5000,
+    port : Number(process.env.PORT) || 5000,
     mongoDBURI : process.env.MONGODB_URI || 'mongodb://localhost:27017/expense-splitter',
     SECRET_KEY : process.env.SECRET_KEY || 'Temp-Secret-Key'
 }
